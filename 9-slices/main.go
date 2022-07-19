f839b62cf4818dd4a9ac80fc1aa6252f7f150538
tigerfsh <mrsmish@yahoo.com>
2022-07-19 17:56:31 +0800

    update at 20220719 17:56

---
+++ 9-slices/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := make([]string, 3)
+	fmt.Println("emp: ", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set: ", s)
+	fmt.Println("get: ", s[2])
+
+	fmt.Println("len: ", len(s))
+
+	s = append(s, "d")
+	fmt.Printf("cap: %d, len: %d\n", cap(s), len(s))
+	s = append(s, "e", "f")
+	fmt.Println("append: ", s)
+	fmt.Printf("cap: %d, len: %d\n", cap(s), len(s))
+
+}
